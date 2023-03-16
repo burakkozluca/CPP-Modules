ce2a231a5b9cd78bc0aa717304f8cc84db0dd96d
Burak Kozluca <bkozluca@k2m14s01.42kocaeli.com.tr>
2023-03-16 10:12:00 +0300

    final

--- Module00/ex01/PhoneBook.cpp
+++
@@ -1,2 +0,0 @@
-#include "PhoneBook.hpp"
-
--- Module00/ex01/main.cpp
+++ Module00/ex01/main.cpp
@@ -17,38 +17,59 @@ int main()
         {   
             while(42)
             {
-                std::cout << "Name:";
+                std::cout << "Enter Name ==>";
                 std::getline(std::cin, p.people[p.sayac].name);
-                std::cout << "Surname:";
+                std::cout << "Enter Surname ==>";
                 std::getline(std::cin, p.people[p.sayac].surname);
-                std::cout << "Nick Name:";
+                std::cout << "Enter Nick Name ==>";
                 std::getline(std::cin, p.people[p.sayac].nickname);
-                std::cout << "Telephone Number:";
+                std::cout << "Enter Telephone Number ==>";
                 std::getline(std::cin, p.people[p.sayac].number);
-                std::cout << "Secret:";
+                std::cout << "Enter Secret ==>";
                 std::getline(std::cin, p.people[p.sayac].secret);
                 if(p.people[p.sayac].name.empty())
                 {
-                    std::cout << "tüm alanlar doldurulmalı" << std::endl;
+                    std::cout << "\033[0;31m\033[1mAll fields must be filled out\033[0m" << std::endl;
                 }
                 else
+                {
                     p.sayac++;
+                    break;
+                }
             }
-            if(p.sayac == 7)
+            if(p.sayac == 8)
                 p.sayac = 0;
         }
         else if(!input.compare("SEARCH"))
         {
+            std::string id;
+            int k;
             int i = 1;
-            std::cout << "\033[0;35m\033[1m                  <>----SEARCH----<>\033[0m" << std::endl;
+            std::cout << "\033[0;35m\033[1m         <>----SEARCH----<>\033[0m" << std::endl;
             while(i <= 8)
             {   
-                std::cout << std::right << std::setw(10) << i << "|";
+                std::cout << std::setw(2) << i;
                 std::cout << std::right << std::setw(10) << (p.people[i - 1].name.size() > 10 ? p.people[i - 1].name.substr(0,9) + "." : p.people[i - 1].name) << "|"; 
                 std::cout << std::right << std::setw(10) << (p.people[i - 1].surname.size() > 10 ? p.people[i - 1].surname.substr(0,9) + "." : p.people[i - 1].surname) << "|";
-                std::cout << std::right << std::setw(10) << (p.people[i - 1].nickname.size() > 10 ? p.people[i - 1].nickname.substr(0,9) + "." : p.people[i - 1].nickname) << "|";
-                std::cout << std::right << std::setw(10) << (p.people[i - 1].number.size() > 10 ? p.people[i - 1].number.substr(0,9) + "." : p.people[i - 1].number) << "|" << std::endl;
+                std::cout << std::right << std::setw(10) << (p.people[i - 1].nickname.size() > 10 ? p.people[i - 1].nickname.substr(0,9) + "." : p.people[i - 1].nickname) << std::endl;
                 i++;
+            }
+            while (42)
+            {
+                std::cout << "Dizin numarası ==>";
+                std::getline(std::cin, id);
+                k = atoi(id.c_str());
+                if(k > 8)
+                    std::cout << "Wrong Index" << std::endl;
+                else
+                {
+                    std::cout << p.people[k - 1].name << std::endl; 
+                    std::cout << p.people[k - 1].surname << std::endl;
+                    std::cout << p.people[k - 1].nickname << std::endl;
+                    std::cout << p.people[k - 1].number << std::endl;
+                    std::cout << p.people[k - 1].secret << std::endl;
+                    break;
+                }
             }
         }
         else if(!input.compare("EXIT"))
@@ -57,7 +78,7 @@ int main()
         }
         else
         {
-
+            std::cout << "\033[0;33m\033[1m<----->Lütfen Bir Komut Girin<----->\033[0m" << std::endl;
         }
     }
 }
