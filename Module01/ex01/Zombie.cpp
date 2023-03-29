c4cae7910f82baefebb301b85a380cc6363193f6
Burak Kozluca <bkozluca@k2m14s06.42kocaeli.com.tr>
2023-03-29 17:15:53 +0300

    moar brainz

---
+++ Module01/ex01/Zombie.cpp
@@ -0,0 +1,17 @@
+#include "Zombie.hpp"
+
+//constructor
+Zombie::Zombie()
+{
+}
+
+//destructor
+Zombie::~Zombie()
+{
+    std::cout << "Zombie has been died" << std::endl;
+}
+
+Zombie* zombieHorde( int N, std::string name )
+{
+    
+}
---
+++ Module01/ex01/Zombie.hpp
@@ -0,0 +1,15 @@
+#ifndef ZOMBIE_HPP
+#define ZOMBIE_HPP
+
+#include <iostream>
+
+class Zombie
+{
+private:
+    std::string name;;
+public:
+    Zombie();
+    ~Zombie();
+};
+
+#endif
