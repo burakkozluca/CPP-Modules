c4eeeae447e0e436e2f9d34996847120cdcf57fd
Burak Kozluca <bkozluca@k2m16s06.42kocaeli.com.tr>
2023-05-10 15:26:20 +0300

    ex02 finished

---
+++ Module01/ex02/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main()
+{
+    std::string a = "HI THIS IS BRAIN";
+    
+    std::string *stringPTR = &a;
+
+    std::string &stringREF = a;
+
+    std::cout << &a << std::endl;
+    std::cout << &stringPTR << std::endl;
+    std::cout << &stringREF << std::endl;
+
+    std::cout << a << std::endl;
+    std::cout << *stringPTR << std::endl;
+    std::cout << stringREF << std::endl;
+}
