baceba1dd057259c889f418e977ae386652c043f
Burak Kozluca <bkozluca@k2m14s03.42kocaeli.com.tr>
2023-05-23 15:12:57 +0300

    harl

--- Module01/ex05/main.cpp
+++ Module01/ex05/main.cpp
@@ -1,7 +1,13 @@
-#include <iostream>
-#include <string.h>
+#include "Harl.hpp"
 
 int main(int argc, char **argv)
 {
-    
+    Harl harl;
+
+    harl.complain("DEBUG");
+    harl.complain("WARNING");
+    harl.complain("ERROR");
+    harl.complain("INFO");
+
+    return(0);
 }
